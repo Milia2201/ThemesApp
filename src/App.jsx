03fefc96afaa1d1d3ffaa0ba68f6c2c7650c4004
Milia Laufenberg <coding.l@ufenberg.net>
2025-07-15 14:36:18 +0200

    part3 done not pretty though

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,41 @@
 import { useState } from "react";
 import "./App.css";
 import Card from "./components/Card.jsx";
-import { themes } from "./assets/db.js";
+import { themes as initialThemes } from "./assets/db.js";
 import Theme from "./components/Theme.jsx";
+import AddThemeForm from "./components/AddThemeForm.jsx";
 
 export default function App() {
-  const [count, setCount] = useState(0);
+  const [themes, setThemes] = useState(initialThemes);
+  console.log("newThemes: ", themes);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+    console.log(data);
+
+    const newTheme = {
+      id: crypto.randomUUID(),
+      name: data.themeName,
+      colors: [
+        { role: "primary", value: data.primary },
+        { role: "secondary", value: data.secondary },
+        { role: "surface", value: data.surface },
+        { role: "surface-on", value: data.surfaceOn },
+      ],
+    };
+
+    setThemes([newTheme, ...themes]);
+    event.target.reset();
+  }
   return (
     <>
       <header>
         <h1 className="title">Theme Creator</h1>
       </header>
       <main className="main">
+        <AddThemeForm handleSubmit={handleSubmit} />
         <ul>
           {themes.map((theme) => {
             return (
